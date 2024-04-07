76e8c1e58675132bfece9a31e6e1698dade142d0
yihhao wang <2633565580@qq.com>
2024-04-07 14:40:36 +0800

    test(hello): complete hello testcase

---
+++ hello_test.go
@@ -0,0 +1,17 @@
+package hello_test
+
+import (
+	"fmt"
+	"github.com/246859/hello"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	data := "jack"
+	expected := fmt.Sprintf("hello %s!", data)
+	result := hello.Hello(data)
+
+	if result != expected {
+		t.Fatalf("expected result %s, but got %s", expected, result)
+	}
+}
